5cf2211de84342666ce8c254e07ebf18a0ff2f45
dennisanthonyblabla <dennisanthonyblabla@gmail.com>
2022-07-21 14:55:41 +0700

    udah ada searchbar

--- CodeBuds/Homepage.swift
+++ CodeBuds/Homepage.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct Homepage: View {
     
+    @State private var searchText = ""
+    
     init() {
         // Use this if NavigationBarTitle is with large font
         UINavigationBar.appearance().largeTitleTextAttributes = [.font : UIFont(name: "Avenir", size: 30)!]
@@ -16,21 +18,24 @@ struct Homepage: View {
         
     var body: some View {
         NavigationView {
-            List(0..<30) { item in
-                VStack (alignment: .leading) {
-                    Text("Make Fake Twitter App")
-                        .font(.custom("Avenir", size: 16))
-                        .fontWeight(.heavy)
-                        .padding(EdgeInsets(top: 5, leading: 0, bottom: 0, trailing: 0))
-                    Text("SwiftUI")
-                        .font(.custom("Avenir", size: 16))
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
-                    Text("By John Doe")
-                        .font(.custom("Avenir", size: 12))
-                        .padding(.bottom, 5)
+            VStack {
+                List(0..<30) { item in
+                    VStack (alignment: .leading) {
+                        Text("Make Fake Twitter App")
+                            .font(.custom("Avenir", size: 16))
+                            .fontWeight(.heavy)
+                            .padding(EdgeInsets(top: 5, leading: 0, bottom: 0, trailing: 0))
+                        Text("SwiftUI")
+                            .font(.custom("Avenir", size: 16))
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
+                        Text("By John Doe")
+                            .font(.custom("Avenir", size: 12))
+                            .padding(.bottom, 5)
+                    }
                 }
+                .searchable(text: $searchText, prompt: "What project were you looking for?")
+                .navigationTitle("Hey, User!")
             }
-        .navigationTitle("Hey, User!")
         }
     }
 }
