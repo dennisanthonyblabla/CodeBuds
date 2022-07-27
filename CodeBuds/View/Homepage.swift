038ffab3e6cb46f7ae4b473741358a7958d93a1d
dennisanthonyblabla <dennisanthonyblabla@gmail.com>
2022-07-27 12:22:17 +0700

    udah selesai coredata Create dan Read dan Delete

--- CodeBuds/View/Homepage.swift
+++ CodeBuds/View/Homepage.swift
@@ -12,11 +12,12 @@ struct Homepage: View {
     @FetchRequest(sortDescriptors: []) var projects: FetchedResults<UProjects>
     
     @State private var searchText = ""
+    @State var isOwner = true
     
     init() {
         // Use this if NavigationBarTitle is with large font
         UINavigationBar.appearance().titleTextAttributes = [.font : UIFont(name: "Avenir Heavy", size: 15)!]
-        UINavigationBar.appearance().largeTitleTextAttributes = [.font : UIFont(name: "Avenir Black", size: 30)!]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.font : UIFont(name: "Avenir Black", size: 30)!, .foregroundColor:UIColor.darkGray]
         UINavigationBar.appearance().backgroundColor = UIColor(Color("BGColor"))
     }
         
@@ -25,43 +26,46 @@ struct Homepage: View {
             ZStack {
                 VStack {
 //                    List(0..<12) { project in
-//                        VStack (alignment: .leading) {
-//                            Text("Make fake twitter app")
-//                                .font(.custom("Avenir Heavy", size: 16))
-//                                .foregroundColor(Color("DarkGray"))
-//                                .padding(EdgeInsets(top: 5, leading: 0, bottom: 0, trailing: 0))
-//                            Text("SwiftUI")
-//                                .font(.custom("Avenir", size: 16))
-//                                .foregroundColor(Color("DarkGray"))
-//                                .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
-//                            Text("By John Doe")
-//                                .font(.custom("Avenir", size: 12))
-//                                .foregroundColor(Color("LightGray"))
-//                                .padding(.bottom, 5)
-//                        }
-///ini buat yang udah connect ke core data
-                    List(projects) { project in
-                        VStack (alignment: .leading) {
-                            Text(project.projectName ?? "unkown")
-                                .font(.custom("Avenir Heavy", size: 16))
-                                .foregroundColor(Color("DarkGray"))
-                                .padding(EdgeInsets(top: 5, leading: 0, bottom: 0, trailing: 0))
-                            Text(project.framework ?? "unknown")
-                                .font(.custom("Avenir", size: 16))
-                                .foregroundColor(Color("DarkGray"))
-                                .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
-                            Text("By John Doe")
-                                .font(.custom("Avenir", size: 12))
-                                .foregroundColor(Color("LightGray"))
-                                .padding(.bottom, 5)
-                        }
-                        .listRowSeparator(.hidden)
+//                        NavigationLink(destination: Articlepage()) {
+//                            VStack (alignment: .leading) {
+//                                Text("Make fake twitter app")
+//                                    .font(.custom("Avenir Heavy", size: 16))
+//                                    .foregroundColor(Color("DarkGray"))
+//                                    .padding(EdgeInsets(top: 5, leading: 0, bottom: 0, trailing: 0))
+//                                Text("SwiftUI")
+//                                    .font(.custom("Avenir", size: 16))
+//                                    .foregroundColor(Color("DarkGray"))
+//                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
+//                                Text("By John Doe")
+//                                    .font(.custom("Avenir", size: 12))
+//                                    .foregroundColor(Color("LightGray"))
+//                                    .padding(.bottom, 5)
+//                            }
+    ///ini buat yang udah connect ke core data
+                        List(projects) { project in
+                            NavigationLink (destination: Articlepage(selectedProject:project, isOwner: isOwner)) {
+                                VStack (alignment: .leading) {
+                                    Text(project.projectName ?? "unkown")
+                                        .font(.custom("Avenir Heavy", size: 16))
+                                        .foregroundColor(Color("DarkGray"))
+                                        .padding(EdgeInsets(top: 5, leading: 0, bottom: 0, trailing: 0))
+                                    Text(project.framework ?? "unknown")
+                                        .font(.custom("Avenir", size: 16))
+                                        .foregroundColor(Color("DarkGray"))
+                                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
+                                    Text("By John Doe")
+                                        .font(.custom("Avenir", size: 12))
+                                        .foregroundColor(Color("LightGray"))
+                                        .padding(.bottom, 5)
+                                }
+                                .listRowSeparator(.hidden)
+                            }
+                        } 
                     }
                     .searchable(text: $searchText, prompt: "What project were you looking for?")
                     .navigationTitle("Hey, User!")
                     
                 }
-            }
 //           .toolbar {
 //               ToolbarItem(placement: .bottomBar) {
 //                   HStack (alignment: .center, spacing: 50
